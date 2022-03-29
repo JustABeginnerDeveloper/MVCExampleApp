d4807edb67976cdecae4daaee442f6b3a2045205
Greg Odintsov <Mynamenoname1996@gmail.com>
2022-03-29 18:35:53 +0300

    Initial Commit

---
+++ MVCExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCExampleApp
+//
+//  Created by Gregory Odintsov on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
